72abd0ad428a5a39d0c195f2bb3ff29ed7ff4490
Trishita Majumder <trishita.majumder15@gmail.com>
2024-01-08 20:56:29 +0530

    chapter 1

---
+++ Chapter 1/App.js
@@ -0,0 +1,16 @@
+const h1 = React.createElement('h1', {}, 'h1 tag')
+const h2 = React.createElement('h2', {}, 'h2 tag')
+const child = React.createElement('div', { id: 'child' }, [h1, h2])
+
+const parent =
+    React.createElement('div', { id: 'parent' },
+        React.createElement('div', { id: 'child' },
+            [
+                React.createElement('h1', {}, 'h1 tag'),
+                React.createElement('h2', {}, 'h2 tag')
+            ]
+        ))
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
