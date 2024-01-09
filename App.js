792766c330e21f80ffea7d17a04c86b60f8a0437
Trishita Majumder <trishita.majumder15@gmail.com>
2024-01-09 15:02:07 +0530

    chapter 2

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-const h1 = React.createElement('h1', {}, 'h1 tag')
-const h2 = React.createElement('h2', {}, 'h2 tag')
-const child = React.createElement('div', { id: 'child' }, [h1, h2])
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// const h1 = React.createElement('h1', {}, 'h1 tag')
+// const h2 = React.createElement('h2', {}, 'h2 tag')
+// const child = React.createElement('div', { id: 'child' }, [h1, h2])
 
 const parent =
     React.createElement('div', { id: 'parent' },
